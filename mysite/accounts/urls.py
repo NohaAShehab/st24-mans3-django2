191bb5490418c91465ba4cbdad7f7eaec347a028
NohaShehab <nohashehab.iti@gmail.com>
2024-10-06 14:39:08 +0300

    registeration

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import include, path
-from accounts.views import  profile
+from accounts.views import  profile, AccountCreatioForm
 
 
 urlpatterns=[
     path("", include("django.contrib.auth.urls")),
-    path("profile/",profile, name='accounts.profile' )
+    path("profile/",profile, name='accounts.profile' ),
+    path("register", AccountCreatioForm.as_view(), name='accounts.register')
 ]
--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.contrib.auth.decorators import  login_required
+from django.views.generic.edit import CreateView
+from accounts.forms import  RegistrationForm
 # Create your views here.
 
 # authenticated user info --> request ??
@@ -8,3 +11,12 @@ from django.contrib.auth.decorators import  login_required
 @login_required()
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+
+class AccountCreatioForm(CreateView):
+    model = User
+    form_class = RegistrationForm
+    template_name = "registration/register.html"
+    success_url = "/accounts/login"
+
+
